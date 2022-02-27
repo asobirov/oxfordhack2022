93cc427a0c4fd625488938b23a5a31cfa9404169
Akbarshokh Sobirov <asobirov357@gmail.com>
2022-02-27 00:46:53 +0000

    feat: add testing data

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import formidable from 'formidable'
+import { getItemFact } from '@helpers/getItemFact';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
@@ -10,8 +11,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             };
 
             case "POST": {
-                const { base64 } = req.body;
-                console.log(base64);
+                // const { base64 } = req.body;
+                console.log(req.body);
+
+                // TODO: send requiest to server
+
+                const prompt = "can";
+
+                res.status(200).json({
+                    success: false,
+                    data: {
+                        prompt,
+                        description: getItemFact(prompt),
+                    }
+                });
             }
         }
     } catch (error: any) {
